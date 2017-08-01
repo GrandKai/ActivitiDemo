0107d3040de4f13ce83add2e322066e389ceefdb
GrandKai <GrandKai@aliyun.com>
2017-08-01 09:48:48 +0800

    Anotation configuration

--- src/test/java/com/activiti/DemoApplicationTests.java
+++ src/test/java/com/activiti/DemoApplicationTests.java
@@ -2,15 +2,24 @@ package com.activiti;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	private WebApplicationContext applicationContext;
+
 	@Test
 	public void contextLoads() {
+
+		for (String s : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
 	}
 
 }
