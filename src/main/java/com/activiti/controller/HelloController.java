07dc3fdad369db4a5162b86725d30efae5f15548
GrandKai <GrandKai@aliyun.com>
2017-08-02 16:03:17 +0800

    thymeleaf模板

--- src/main/java/com/activiti/controller/HelloController.java
+++ src/main/java/com/activiti/controller/HelloController.java
@@ -1,14 +1,20 @@
 package com.activiti.controller;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String index() {
-        return "Hello World";
+    public ModelAndView index(ModelMap map) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        map.put("host", "http://www.baidu.com");
+//        modelAndView.setView(new Vi);
+        return modelAndView;
     }
 
 }
